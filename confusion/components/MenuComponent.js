3cd8b3854d1ad43b6336a3ae96e778835ed1f33a
Sneh <snehkoul1999@gmail.com>
2018-08-18 17:36:02 -0400

    Completed Stack Navigation

--- confusion/components/MenuComponent.js
+++ confusion/components/MenuComponent.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React , {Component} from 'react';
 import {View,FlatList} from 'react-native';
 import {ListItem} from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
 
-function Menu(props) {
+
+
+class Menu extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
+     static navigationOptions = {
+        title: 'Menu'
+    };
+
+    render(){
+
+        const {navigate} = this.props.navigation;
 
     const renderMenuItem = ({item,index}) => {
           return (
@@ -11,7 +28,7 @@ function Menu(props) {
                     title={item.name}
                     subtitle={item.description}
                     hideChevron={true}
-                    onPress={() => props.onPress(item.id)}
+                     onPress={() => navigate('Dishdetail', { dishId: item.id })}
                     leftAvatar={{ source: require('./images/uthappizza.png')}}
                   />
         );
@@ -19,11 +36,13 @@ function Menu(props) {
 
      return (
             <FlatList 
-                data={props.dishes}
+                data={this.state.dishes}
                 renderItem={renderMenuItem}
                 keyExtractor={item => item.id.toString()}
                 />
     );
+}
+    
 }
 
 
--- confusion/shared/dishes.js
+++ confusion/shared/dishes.js
@@ -20,24 +20,24 @@ export const DISHES =
         featured: false,
         description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
         },
-        // {
-        // id: 2,
-        // name:'Vadonut',
-        // image: 'images/vadonut.png',
-        // category: 'appetizer',
-        // label:'New',
-        // price:'1.99',
-        // featured: false,
-        // description:'A quintessential ConFusion experience, is it a vada or is it a donut?'
-        // },
-        // {
-        // id: 3,
-        // name:'ElaiCheese Cake',
-        // image: 'images/elaicheesecake.png',
-        // category: 'dessert',
-        // label:'',
-        // price:'2.99',
-        // featured: false,
-        // description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
-        // }
+        {
+        id: 2,
+        name:'Vadonut',
+        image: 'images/vadonut.png',
+        category: 'appetizer',
+        label:'New',
+        price:'1.99',
+        featured: false,
+        description:'A quintessential ConFusion experience, is it a vada or is it a donut?'
+        },
+        {
+        id: 3,
+        name:'ElaiCheese Cake',
+        image: 'images/elaicheesecake.png',
+        category: 'dessert',
+        label:'',
+        price:'2.99',
+        featured: false,
+        description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
+        }
     ];
